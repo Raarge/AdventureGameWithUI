133f6bb09a6c153cc02b1a78640d932830d141d1
Darin Campbell - Raarge Actual <animusdrogai@gmail.com>
2024-01-01 02:59:54 -0500

    Added the deserialization constructor to RoomList class to make it work.

--- Game04/gameclasses/RoomList.cs
+++ Game04/gameclasses/RoomList.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,11 @@ namespace Game04.gameclasses
     [Serializable]
     public class RoomList : Dictionary<Rm, Room>
     {
+        protected RoomList(SerializationInfo info, StreamingContext context)
+       : base(info, context)
+        {
+            // constructor needed for serialization
+        }
         public RoomList() { }
 
         public Room RoomAt(Rm id)
